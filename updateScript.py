6ad30e2f75b9a51782804df93516f649298db977
Tu1026 <64434778+Tu1026@users.noreply.github.com>
2020-07-12 02:09:43 -0700

    Creating a psuedo website
    Using reference https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
    to create a template website
    (SQL + migration + form +database)

--- updateScript.py
+++ updateScript.py
@@ -1,5 +1,4 @@
 """Monitors the given course and send update when a spot frees up
-
     Usage: change the url to the course, chagne the word looking for to however, many people is registered in the class
     , subscribe to the printed website to recieve notification
 """
@@ -15,10 +14,29 @@ from configparser import ConfigParser
 from dotenv import load_dotenv
 import os
 import winsound
+from fbchat import Client
+import fbchat
 
 
 load_dotenv()
-s = os.getenv("password")
+# use https://findmyfbid.in/#:~:text=Find%20your%20facebook%20ID%20in%20two%20easy%20steps&text=Your%20Facebook%20personal%20profile%20URL,profile.php%3Fid%3D100001533612613
+# to find uid
+def send_fb_message(word: str, uid: str):
+    """print the given string one word at a time to fb friend
+        Args: 
+            a string that contain multiple words, the uid of the fb friend
+            user input: username and password
+    """
+    uid = os.getenv("uid")
+    username_fb = os.getenv("username2") 
+    passwrod_fb = os.getenv("password1")
+    username = str(input("Username: ")) 
+    client = fbchat.Client(username_fb, passwrod_fb)
+    client.send(fbchat.models.Message(word), uid)
+    client.logout()
+
+
+
 #Reference from https://stackabuse.com/how-to-send-emails-with-gmail-using-python/
 def send_email(username, password):
     """send email to the person who wishes to recieve notification
@@ -59,9 +77,9 @@ while True:
     word_looking_for = "Registered:" + registered
     # if the amount of people registered has not changed keep looping
     if word_looking_for in text_list:
-        # wait 30 seconds,
-        print("No seats avaliable yet updating in 30 seconds")
-        time.sleep(30)
+        # wait 15 seconds,
+        print("No seats avaliable yet updating in 15 seconds")
+        time.sleep(15)
         # continue with the script,
         continue
         
@@ -74,13 +92,11 @@ while True:
             config.read('config.ini')
             username = os.getenv("username1")
             password = os.getenv("password")
-            # username = config.get("email", "username")
-            # password = config.get("email", "password")
+            send_fb_message("register for " + course + "NOWWWWWWW")
             send_email(username, password)
             print("email notificaiton sent")
         except:
-            print("something went wrong with emailing stuff")
+            print("something went wrong with emailing stuff or FB stuff")
         ctypes.windll.user32.MessageBoxW(0, course, 'Spot is now open for', course)
         winsound.MessageBeep()
         break
-
